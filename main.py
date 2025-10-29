e549f5815c99a17613f223f81eec402f7573bdc8
Ziad Balbzioui <ziad.balbzioui@etu.minesparis.psl.eu>
2025-10-29 23:15:34 +0100

    v2

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ snake = [[1,1], [2,1], [3,1]]
 
 perdu = False
 fruit = [random.randint(0,WIDTH),random.randint(0,HEIGHT)]
+score = 0 
 
 while not window_should_close() and not perdu: 
     begin_drawing()
@@ -38,16 +39,20 @@ while not window_should_close() and not perdu:
     if new_head == fruit : 
         snake = snake + [new_head]
         fruit = [random.randint(0,WIDTH-1),random.randint(0,HEIGHT-1)]
-
+        score += 1
     else : 
         snake = snake[1:] + [new_head]
     
 
     #Condition de fin de partie
-    if new_head[0] >= WIDTH or new_head[0] < 0: 
-         perdu = True
-    if new_head[1] >= HEIGHT or new_head[1] < 0: 
-        perdu = True
+    if new_head[0] >= WIDTH :  
+        new_head[0] = 0
+    if new_head[0] < 0 : 
+        new_head[0] = WIDTH - 1
+    if new_head[1] >= HEIGHT : 
+        new_head[1] = 0
+    if new_head[1] < 0 : 
+        new_head[1] = HEIGHT 
     if new_head in snake[:-1] : 
         perdu = True
             
