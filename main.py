5879daf42f265e8690e24185fb1c2ebc998203d2
Ziad Balbzioui <ziad.balbzioui@etu.minesparis.psl.eu>
2025-11-02 20:39:44 +0100

    v3

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ vitesse = [1,0]
 snake = [[1,1], [2,1], [3,1]]
 
 perdu = False
+
+
 fruit = [random.randint(0,WIDTH),random.randint(0,HEIGHT)]
 score = 0 
 
-while not window_should_close() and not perdu: 
+screen_width = get_monitor_width(0)
+screen_height = get_monitor_height(0)
+
+while not window_should_close() : 
     begin_drawing()
     clear_background(RAYWHITE)
 
@@ -31,20 +36,23 @@ while not window_should_close() and not perdu:
     
 
     #ANIMATION DU SERPENT
-    vx, vy = vitesse
-    hx, hy = snake[-1]
-    new_head = [hx+ vx, hy+vy]
+    if not perdu : 
+        vx, vy = vitesse
+        hx, hy = snake[-1]
+        new_head = [hx+ vx, hy+vy]
+
+        if new_head == fruit : 
+            snake = snake + [new_head]
+            fruit = [random.randint(0,WIDTH-1),random.randint(0,HEIGHT-1)]
+            score += 1
+        else : 
+            snake = snake[1:] + [new_head]
+        
+        draw_rectangle(fruit[0]*SIDE,fruit[1]*SIDE,SIDE-2,SIDE-2,RED)
+        draw_text("Score : "+ str(score), 10, 10, 30, GRAY)
 
 
-    if new_head == fruit : 
-        snake = snake + [new_head]
-        fruit = [random.randint(0,WIDTH-1),random.randint(0,HEIGHT-1)]
-        score += 1
-    else : 
-        snake = snake[1:] + [new_head]
-    
 
-    #Condition de fin de partie
     if new_head[0] >= WIDTH :  
         new_head[0] = 0
     if new_head[0] < 0 : 
@@ -53,12 +61,28 @@ while not window_should_close() and not perdu:
         new_head[1] = 0
     if new_head[1] < 0 : 
         new_head[1] = HEIGHT 
+
+    #Condition de fin de partie
     if new_head in snake[:-1] : 
         perdu = True
+        
+    
+    if perdu : 
+
+        draw_rectangle(0,0,screen_width,screen_height,BLACK)
+        draw_text("Votre score est de : " + str(score),10,  10, 50, WHITE)
+        draw_text("Presser sur entrer pour réessayer",10,  60, 20, WHITE)
+
+        if is_key_pressed(KEY_ENTER) : 
+            perdu = False
+            snake = snake[len(snake)-4:]
+            score = 0
             
-    draw_rectangle(fruit[0]*SIDE,fruit[1]*SIDE,SIDE-2,SIDE-2,RED)
+    #Dessin du serpent
     for (x,y) in snake : 
         draw_rectangle(x*SIDE+1,y*SIDE+1,SIDE-2,SIDE-2,GREEN)
+    draw_rectangle(new_head[0]*SIDE +1, new_head[1]*SIDE + 1, SIDE-2, SIDE- 2, DARKGREEN)
+
 
     end_drawing()
 
